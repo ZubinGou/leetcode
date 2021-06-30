eb19f9e2725b4db430b457636fe3614f99332172
ZubinGou <zebgou@gmail.com>
2021-06-30 12:50:09 +0800

    224

--- 224-Basic_Calculator.cpp
+++ 224-Basic_Calculator.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stack>
 
 using namespace std;
 
 int calculate(string s) {
-    int total = 0;
-    vector<int> signs(1, 1);
-    for (int i=0; i<s.size(); i++) {
+    int res = 0;
+    int sign_now = 1;
+    stack<int> sign;
+    sign.push(1);
+    
+    for (int i = 0; i < s.length(); i++) {
         char c = s[i];
-        if (c >= '0') {
+        if (c >= '0' && c <= '9') {
             int number = 0;
-            while (i < s.size()  &&  s[i] >= '0')
-                number = 10 * number + s[i++] - '0';
-            total += signs.back() * number;
-            signs.pop_back();
+            while (i < s.size() && s [i] >= '0')
+                number = 10 * number - '0' + s[i++];
             i--;
+            res += number * sign_now * sign.top();
         }
-        else if (c == ')')
-            signs.pop_back();
-        else if (c == '+' || c == '-')
-            signs.push_back(signs.back() * (c == '-' ? -1 : 1));
+        else if (c == '+')
+            sign_now = 1;
+        else if (c == '-') 
+            sign_now = -1;
+        else if (c == '(') {
+            sign.push(sign.top() * sign_now);
+            sign_now = 1;
+        }
+        else if (c == ')') {
+            sign.pop();
+        }
+        cout << "res = " << res << endl;
     }
-    return total;
+    return res;
 }
 
+
 int main() {
-    string s(" -(-1)-1");
-    cout << calculate(s);
+    // string s(" -(-1)-1");
+    // string s("1- -1");
+    // string s("2147483647");
+    string s("- (3 + (4 + 5))");
+    cout << calculate(s) << endl;
     return 0;
 }
