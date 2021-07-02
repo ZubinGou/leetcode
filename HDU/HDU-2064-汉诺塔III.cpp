d5a1f773ffaa3c05798222c2ecf83e6d5ddd57a0
ZubinGou <zebgou@gmail.com>
2021-07-02 09:07:17 +0800

    HDU-2064

--- HDU/HDU-2064-汉诺塔III.cpp
+++ HDU/HDU-2064-汉诺塔III.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
 int main() {
+    int n;
+    // first calc all res
+    vector<int> dp(36, 0);
+    for (int i = 1; i <= 35; i++)
+        dp[i] = dp[i - 1] * 3 + 2;
 
+    while (cin >> n)
+        cout << dp[n] << endl;   
+
+    return 0;
+}
