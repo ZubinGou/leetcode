1a0a75b1b9063791925e45284a25e0aed6e04b57
ZubinGou <zebgou@gmail.com>
2021-07-28 12:20:46 +0800

    offer 1

--- 剑指offer/1-赋值运算符函数.cpp
+++ 剑指offer/1-赋值运算符函数.cpp
@@ -1,73 +1,147 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <map>
-// #include <unordered_map>  // can't compile on poj
-#include <algorithm>
-#include <cstring>
-#include <climits>
+/*******************************************************************
+Copyright(c) 2016, Harry He
+All rights reserved.
+Distributed under the BSD license.
+(See accompanying file LICENSE.txt at
+https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
+*******************************************************************/
 
-using namespace std;
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题1：赋值运算符函数
+// 题目：如下为类型CMyString的声明，请为该类型添加赋值运算符函数。
+
+#include<cstring>
+#include<cstdio>
 
 class CMyString
 {
 public:
-    CMyString(char *pData = nullptr)
-    {
-        m_pData = pData;
-    };
+    CMyString(char* pData = nullptr);
+    CMyString(const CMyString& str);
+    ~CMyString(void);
 
-    CMyString(const CMyString &str)
-    {
-        m_pData = str.m_pData;
-    };
+    CMyString& operator = (const CMyString& str);
 
-    ~CMyString()
-    {
-        delete[] m_pData;
-        m_pData = nullptr;
-    };
-    // Solution 1
-    CMyString &operator=(const CMyString &str)
-    {
-        if (this != &str)
-        {
-            delete[] m_pData;
-            m_pData = nullptr;
-
-            m_pData = new char[strlen(str.m_pData) + 1];
-            strcpy(m_pData, str.m_pData);
-        }
-        return *this;
-    }
-
+    void Print();
+      
 private:
-    char *m_pData;
+    char* m_pData;
 };
 
-// // Solution 2
-// CMyString &CMyString::operator=(const CMyString &str)
-// {
-//     if (this != &str)
-//     {
-//         CMyString strTemp(str);
-//         char *pTemp = strTemp.m_pData;
-//         strTemp.m_pData = m_pData;
-//         m_pData = pTemp;
-//     }
-//     return *this;
-// }
-
-void _test()
+CMyString::CMyString(char *pData)
 {
-    CMyString str1((char *)"heyyy!!");
-    CMyString str2(str1);
-    CMyString str3 = str1;
+    if(pData == nullptr)
+    {
+        m_pData = new char[1];
+        m_pData[0] = '\0';
+    }
+    else
+    {
+        int length = strlen(pData);
+        m_pData = new char[length + 1];
+        strcpy(m_pData, pData);
+    }
 }
 
-int main()
+CMyString::CMyString(const CMyString &str)
 {
-    _test();
+    int length = strlen(str.m_pData);
+    m_pData = new char[length + 1];
+    strcpy(m_pData, str.m_pData);
+}
+
+CMyString::~CMyString()
+{
+    delete[] m_pData;
+}
+
+CMyString& CMyString::operator = (const CMyString& str)
+{
+    if(this == &str)
+        return *this;
+
+    delete []m_pData;
+    m_pData = nullptr;
+
+    m_pData = new char[strlen(str.m_pData) + 1];
+    strcpy(m_pData, str.m_pData);
+
+    return *this;
+}
+
+// ====================测试代码====================
+void CMyString::Print()
+{
+    printf("%s", m_pData);
+}
+
+void Test1()
+{
+    printf("Test1 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    CMyString str2;
+    str2 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str2.Print();
+    printf(".\n");
+}
+
+// 赋值给自己
+void Test2()
+{
+    printf("Test2 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    str1 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str1.Print();
+    printf(".\n");
+}
+
+// 连续赋值
+void Test3()
+{
+    printf("Test3 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    CMyString str2, str3;
+    str3 = str2 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str2.Print();
+    printf(".\n");
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str3.Print();
+    printf(".\n");
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+
     return 0;
 }
